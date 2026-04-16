c31d38aacb731eaa6ab2cf99d655a68b07190d2b
Samien <samienshaheed@gmail.com>
2026-04-17 01:30:49 +0600

    Add project files.

---
+++ Physics_Engine/main.cpp
@@ -0,0 +1,57 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// Global Variables
+int WINDOW_WIDTH = 800;
+int WINDOW_HEIGHT = 600;
+
+// Function to adjust the viewport when the window is resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+// Function to process input (like keyboard events)
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Physics Engine", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // main rendering loop
+    while (!glfwWindowShouldClose(window)) {
+
+        processInput(window); // input checking
+
+		// rendering commands here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+		// check and call events and swap the buffers
+		glfwSwapBuffers(window); // swap the color buffers (double buffering)
+		glfwPollEvents(); // check for events (like keyboard input, mouse movement, etc.)
+    }
+
+    glfwTerminate();
+    return 0;
+}
